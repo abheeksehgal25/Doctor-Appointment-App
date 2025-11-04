722b9db43780d932946ed7ea92e13a8f75ae0d80
abheeksehgal25 <sehgalabheek@gmail.com>
2025-11-04 23:00:15 +0530

    added the admin page url

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -46,7 +46,7 @@ const Navbar = () => {
         {/* ✅ Admin Panel Button - show only on home page  */}
         {location.pathname === '/' && (
           <button
-            onClick={() => window.open('https://doctor-appointment-web-app-9iz9.vercel.app/', '_blank')}
+            onClick={() => window.open('https://doctor-appointment-app-5msi.vercel.app/', '_blank')}
             className='bg-primary text-white text-xs px-4 py-2 rounded-full hover:bg-gray-700 hidden md:block'
           >
             Admin Panel
